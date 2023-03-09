44e674ec970a784ec065cb9486f7ef154b12386f
Maria Hildebrandt <hildebrandt.maria@gmail.com>
2023-03-09 14:58:16 +0100

    changed photo

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -10,9 +10,6 @@ const Navbar = () => {
   return (
     <nav>
       <div className="container nav__container">
-        <a href="index.html" className='nav__logo'>
-          <img src={Logo} alt="Logo" />
-        </a>
         <ul className='nav__menu'>
           {
             data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
